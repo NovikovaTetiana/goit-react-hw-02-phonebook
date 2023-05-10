7d7e3dd79b509e0f954daa27ba2a8cb983f3a81c
Tetiana Novikova <113465579+NovikovaTetiana@users.noreply.github.com>
2023-05-11 02:26:56 +0300

    hw-2 phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { Container, Title, SubTitle } from './App.styled'
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = data => {
+    const { contacts } = this.state;
+    const newContact = {
+      id: nanoid(),
+      ...data,
+    };
+
+    contacts.some(({ name }) => name === data.name)
+      ? alert(`${data.name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, newContact],
+        }));
+
+    console.log(data);
+  };
+
+  handleDeleteContact = userId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== userId),
+    }));
+  };
+
+  handleChangeFilter = e => this.setState({ filter: e.target.value });
+
+  getFilterContacts = () => {
+    const { filter, contacts } = this.state;
+    const filterlowerCase = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filterlowerCase)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <Container>
+        <Title>Phonebook</Title>
+        <ContactForm onSubmit={this.addContact} />
+
+        <SubTitle>Contacts </SubTitle>
+        <Filter value={filter} onChange={this.handleChangeFilter} />
+        <ContactList
+          contacts={this.getFilterContacts()}
+          onDelete={this.handleDeleteContact}
+        />
+      </Container>
+    );
+  }
+}
---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const List= styled.li`
+display:flex;
+flex-direction: column;
+gap:12px;
+font-size:20px
+`
+
+
